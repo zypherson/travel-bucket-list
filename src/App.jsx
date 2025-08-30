9a384a81808301fe99093fe1ca0fd6e6ac54cde9
zypherson <taylorzypherson@gmail.com>
2025-08-30 01:36:50 -0400

    scaffolded navbar and added it to  homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css'
+import Navbar from "./components/Navbar";
 
 
 export default function App() {
   return (
     <div className="app">
+      <Navbar />
       <h1>Travel Bucket List</h1>
       <p>Travel freely</p>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+      <nav className="navbar">
+        <h2 className="logo">Travel Bucket List</h2>
+      </nav>
+    );
+  }
