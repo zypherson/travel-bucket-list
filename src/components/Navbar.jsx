a491afa53b30a8cbcec228c0e29cf06a1fd07178
zypherson <taylorzypherson@gmail.com>
2025-09-02 12:14:14 -0400

    added home,destinations,about on the nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 export default function Navbar() {
     return (
-      <nav className="navbar">
+        <nav className="navbar">
         <h2 className="logo">Travel Bucket List</h2>
+        <ul className="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Destinations</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
       </nav>
     );
   }
