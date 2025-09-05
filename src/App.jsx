833d51ec227689dce2a24141630074d85f662a56
zypherson <taylorzypherson@gmail.com>
2025-09-05 03:02:20 -0400

    set up routes to home,about,destinations

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import './App.css'
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
+function Home() {
+  return <h1>Welcome to Your Travel Bucket List 🌍</h1>;
+}
+
+function Destinations() {
+  return <h1>Explore Destinations ✈️</h1>;
+}
+
+function About() {
+  return <h1>About This App 📝</h1>;
+}
 
 export default function App() {
   return (
-    <div className="app">
+    <>
       <Navbar />
-      
-      <h1>Travel Bucket List</h1>
-      <p>Travel freely</p>
-    
-    </div>
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destinations" element={<Destinations />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
