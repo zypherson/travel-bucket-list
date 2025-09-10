8cc8a942dcddd2b235f947c17222418ba902b94e
zypherson <taylorzypherson@gmail.com>
2025-09-10 12:06:29 -0400

    added actual link functionality to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-    return (
-        <nav className="navbar">
-        <h2 className="logo">Travel Bucket List</h2>
-        <ul className="nav-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Destinations</a></li>
-          <li><a href="#">About</a></li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar">
+      <h2 className="logo">Travel Bucket List</h2>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/destinations">Destinations</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+}
