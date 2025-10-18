0a019db353d75c6654e463f29dcaf80b7603dddf
zypherson <taylorzypherson@gmail.com>
2025-10-18 00:59:11 -0400

    styled modal list

--- src/components/EditDestinationModal.jsx
+++ src/components/EditDestinationModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import './Modal.css'
 
 export default function EditDestinationModal({ destination, onSave, onClose }) {
   const [formData, setFormData] = useState({
